c88876dc72038ed6aa7a87c817be1b38d7afa1fa
Mahfujur <xmahfujur@gmail.com>
2026-04-24 09:51:18 +0600

    nw

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { initializeFirestore, doc, getDocFromServer } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import firebaseConfig from '../firebase-applet-config.json';
+
+const app = initializeApp(firebaseConfig);
+
+// Use experimentalForceLongPolling to fix "Could not reach Cloud Firestore backend" in restricted networks
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+}, firebaseConfig.firestoreDatabaseId);
+
+export const auth = getAuth(app);
+export const storage = getStorage(app);
